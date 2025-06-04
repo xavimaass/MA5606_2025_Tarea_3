2eeb8ca11deaafd6eed2da1d83a32a3bf6e5526e
XaviMaass <javier.maass@gmail.com>
2025-06-03 22:42:38 -0400

    feat: euler-maruyama predictor

--- src/sde.py
+++ src/sde.py
@@ -2,6 +2,8 @@ import abc
 from typing import Any
 import torch
 
+
+## SDE abstract class
 class SDE(abc.ABC):
     """SDE abstract class. Functions are designed for a mini-batch of inputs."""
     def __init__(self, N: int):
@@ -83,3 +85,30 @@ class SDE(abc.ABC):
 
         sde_backward = RSDE()
         return sde_backward
+
+
+## Euler Maruyama Predictor 
+class PredictorPT(abc.ABC):
+    def __init__(self, sde_obj):
+        self.sde = sde_obj
+
+    @abc.abstractmethod
+    def update_fn(self, t: torch.Tensor, x: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
+        pass
+
+class EulerMaruyamaPredictorPT(PredictorPT):
+    def __init__(self, sde_obj):
+        super().__init__(sde_obj)
+
+    def update_fn(self, t, x): # t is current time, x is current state
+        dt = 1.0 / self.sde.N # Timestep size (reverse SDE uses N steps over T)
+
+        # Get drift and diffusion from the SDE (could be reverse SDE)
+        # model prediction (score) is handled inside sde.sde_coeff if it's an RSDE instance
+        drift, diffusion_g = self.sde.sde_coeff(t, x)
+
+        z = torch.randn_like(x)
+        x_mean = x + dt * drift # Euler step for mean
+        x_next = x_mean + diffusion_g * math.sqrt(dt) * z # Add noise scaled by g(t) and sqrt(dt)
+
+        return x_next, x_mean
